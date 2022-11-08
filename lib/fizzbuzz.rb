877c3d70d7b4eb8ffdaffa0d5db0dc53ac336ff8
Samir70 <sbetmouni@talktalk.net>
2022-11-08 18:03:15 +0000

    add code and tests for fizzbuzz
    Co-authored-by: Roi <oceansize@users.noreply.github.com>

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,3 +5,21 @@ end
 def is_divisible_by_five?(number)
   return (number % 5) == 0
 end
+
+def fizzbuzz(number)
+  raise "ERROR: Not an integer" unless number.instance_of?(Integer)
+
+  if(is_divisible_by_three?(number) && is_divisible_by_five?(number)) 
+    return "fizzbuzz"
+  end
+
+  if(is_divisible_by_three?(number))
+    return "fizz"
+  end
+
+  if(is_divisible_by_five?(number))
+    return "buzz"
+  end
+
+  return number.to_s
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe 'FizzBuzz' do
       expect(is_divisible_by_five?(37)).to eq(false)
     end
   end
+  context "Given a fizzy number" do
+    it "returns fizz" do
+      expect(fizzbuzz(12)).to eq "fizz"
+    end
+  end
+  context "Given a buzzy number" do
+    it "returns buzz" do
+      expect(fizzbuzz(20)).to eq "buzz"
+    end
+  end
+  context "Given a fizzbuzzy number" do
+    it "returns fizzbuzz" do
+      expect(fizzbuzz(15)).to eq "fizzbuzz"
+    end
+  end
+  context "Given a ordinary number" do
+    it "returns the number as a string" do
+      expect(fizzbuzz(16)).to eq "16"
+    end
+  end
+
+  context "If not given a number" do
+    it "raise an error" do
+      expect { fizzbuzz("hello")}.to raise_error "ERROR: Not an integer"
+    end
+  end
 end
